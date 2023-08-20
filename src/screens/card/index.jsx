cce1ebef6b2267b6063dbd7f23d7e75d9063c581
Garima Gupta <garima.gupta1402@gmail.com>
2023-08-20 22:49:23 +0530

    detail page ui & api  integrated

--- src/screens/card/index.jsx
+++ src/screens/card/index.jsx
@@ -5,11 +5,11 @@ import axios from "axios";
 import { useNavigate } from "react-router";
 function Card() {
   const navigate = useNavigate();
-  const [title, setTitle] = useState();
-
+  const [data, setData] = useState();
+  const [page, setPage] = useState(1);
   const upcoming = {
     method: "GET",
-    url: "https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1",
+    url: `https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=${page}`,
     headers: {
       Authorization:
         "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiM2M2N2JkNGJlOGZiNzI3MWQ0Mjk2ZjVmYjIxZGI1OSIsInN1YiI6IjY0ZTA1N2IzYTNiNWU2MDFkNTllNDBmOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jL-9-aGTZekmss2g8FyA8FCJUW1_vbMIHkyffCYb1gE",
@@ -21,45 +21,13 @@ function Card() {
     axios
       .request(upcoming)
       .then(function (response) {
-        setTitle(response.data.results);
+        setData(response.data.results);
         console.log(response.data.results, "resssssssssss");
       })
       .catch(function (error) {
         console.error(error);
       });
   };
-  //     axios
-  //       .request(options2020)
-  //       .then(function (response) {
-  //         setTitle2020(response.data.results);
-  //         console.log(response.data.results);
-  //       })
-  //       .catch(function (error) {
-  //         console.error(error);
-  //       });
-  //   };
-  //   const get2021 = () => {
-  //     axios
-  //       .request(options2021)
-  //       .then(function (response) {
-  //         setTitle2021(response.data.results);
-  //         console.log(response.data.results);
-  //       })
-  //       .catch(function (error) {
-  //         console.error(error);
-  //       });
-  //   };
-  //   const get2022 = () => {
-  //     axios
-  //       .request(options2022)
-  //       .then(function (response) {
-  //         setTitle2022(response.data.results);
-  //         console.log(response.data.results);
-  //       })
-  //       .catch(function (error) {
-  //         console.error(error);
-  //       });
-  //   };
   useEffect(() => {
     getUpcoming();
   }, []);
@@ -69,11 +37,10 @@ function Card() {
       <div className="container">
         <div className="heading-wrapper">
           <h2 className="main-text">Upcoming Series</h2>
-          {/* <h2 className="sub-text">10 entries with carousel</h2> */}
         </div>
 
         <div className="card-box">
-          {title?.map((item, index) => {
+          {data?.map((item, index) => {
             return (
               <div
                 key={index}
@@ -92,7 +59,7 @@ function Card() {
                   alt="No Image"
                 />
                 <div className="movie-des">
-                  <h3 className="image-name">{item?.original_title}</h3>
+                  <h3 className="image-name">{item?.original_data}</h3>
                   <p>{item.vote_average}</p>
                 </div>
                 <p style={{ margin: "12px " }}>{item?.overview}</p>
--- src/screens/details/index.jsx
+++ src/screens/details/index.jsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
-// import Description from "./description";
 import "./details.css";
-// import Title from "./title";
 import axios from "axios";
 
 function Details() {
@@ -22,8 +20,8 @@ function Details() {
     axios
       .request(options)
       .then(function (response) {
-        console.log(response.data.results, "result");
-        setDetail(response.data.results);
+        console.log(response.data, "result");
+        setDetail(response.data);
       })
       .catch(function (error) {
         console.error(error);
@@ -32,6 +30,46 @@ function Details() {
   useEffect(() => {
     getDetail();
   }, []);
-  return <div>I am here {id}</div>;
+  const date = new Date(detailData?.release_date);
+  const year = date.getFullYear();
+
+  const totalMinutes = detailData?.runtime;
+  const hours = Math.floor(totalMinutes / 60);
+  const minutes = totalMinutes % 60;
+
+  console.log(`${hours} hours and ${minutes} minutes`);
+
+  return (
+    <div className="wrapper">
+      <div className="image-container">
+        <img
+          className="image-detail"
+          src={
+            detailData?.poster_path
+              ? `https://image.tmdb.org/t/p/w220_and_h330_face${detailData?.poster_path}`
+              : "noImage.png"
+          }
+          alt="myImg"
+        />
+      </div>
+      <div className="container">
+        <div className="title-container">
+          <h1 className="title">{detailData?.title}</h1>
+          <h1 className="rating">
+            {detailData ? `(${detailData?.vote_average})` : ""}
+          </h1>
+        </div>
+
+        <div className="des-container">
+          <p>
+            {year} | {`${hours} Hr ${minutes} min`} | {detailData?.tagline}
+          </p>
+          {/* <p>{`${hours} Hr ${minutes} min`}</p>
+          <p>{detailData?.tagline}</p> */}
+        </div>
+        <p className="des-text">{detailData?.overview}</p>
+      </div>
+    </div>
+  );
 }
 export default Details;
